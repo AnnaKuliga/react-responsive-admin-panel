320227cf78f806ca48785938a814e997d325750b
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-05-04 17:45:47 +0200

    Added menu Items and styled the menu bar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Sidebar.css";
+import Logo from "../imgs/logo.png";
+import { SidebarData } from "../data/Data";
+import { UilSignOutAlt } from "@iconscout/react-unicons";
+
+const Sidebar = () => {
+  return (
+    <div className="Sidebar">
+      {/*logo*/}
+      <div className="logo">
+        <img src={Logo} alt="" />
+        <span>
+          Sh<span>o</span>ps
+        </span>
+      </div>
+      {/*menu*/}
+      <div className="menu">
+        {SidebarData.map((item, index) => {
+          return (
+            <div className="menuItem">
+              <item.icon />
+              <span>{item.heading}</span>
+            </div>
+          );
+        })}
+        <div className="menuItem">
+          <UilSignOutAlt />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
