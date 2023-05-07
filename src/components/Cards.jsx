5cd336d17dc2c6889f9cfb670c45ee4e5a235835
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-05-07 19:20:29 +0200

    removed Cards element

--- src/components/Cards.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Cards.css";
-
-const Cards = () => {
-  return <div className="Cards"></div>;
-};
-
-export default Cards;
--- src/components/MainDash.jsx
+++ src/components/MainDash.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./MainDash.css";
-import Cards from "./Cards";
+
 const MainDash = () => {
   return (
     <div className="MainDash">
       <h1>Dashboard</h1>
-      <Cards />
     </div>
   );
 };
