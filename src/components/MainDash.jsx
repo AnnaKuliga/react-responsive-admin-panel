0a79845fa172d950129defb1dba60c56adce22a6
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-06-15 19:17:51 +0200

    created Cards component in the dashboard section

--- src/components/MainDash.jsx
+++ src/components/MainDash.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./MainDash.css";
+import Cards from "./Cards";
 
 const MainDash = () => {
   return (
     <div className="MainDash">
       <h1>Dashboard</h1>
+      <Cards />
     </div>
   );
 };
